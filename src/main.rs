315ddf8009b6a4261bb2827f50ec0569671f5435
James Waples <james@wapl.es>
2019-12-22 17:10:53 +0000

    Shitty solution to DMA issues

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@
 
 use panic_semihosting as _;
 
-use cortex_m::asm::wfi;
+use cortex_m::{asm::wfi, singleton};
 use rtfm::app;
 
 use embedded_hal::digital::v2::OutputPin;
 use stm32f1xx_hal::{
     device::SPI2,
+    dma,
     gpio::{gpiob, gpioc::PC13, Output, PushPull, State},
     pac,
     prelude::*,
@@ -38,14 +39,17 @@ type DmaInterface = stm32f1xx_hal::dma::TxDma<
     stm32f1xx_hal::dma::dma1::C5,
 >;
 
-static mut DATA: [u8; (64 * 8 * 3) + 1] = [OFF_BYTE; (64 * 8 * 3) + 1];
+const DATA_LEN: usize = (64 * 8 * 3) + 1;
+static mut DATA: [u8; DATA_LEN] = [OFF_BYTE; DATA_LEN];
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
 const APP: () = {
     struct Resources {
         led: PC13<Output<PushPull>>,
-        spi_dma: DmaInterface,
+        spi_dma: &'static mut DmaInterface,
         timer_handler: CountDownTimer<pac::TIM1>,
+        timer2_handler: CountDownTimer<pac::TIM2>,
+        // buf: &'static mut [u8; DATA_LEN],
     }
     // static mut SPI_DEVICE: PC13<Output<PushPull>> = ();
     // static mut TIMER_HANDLER: CountDownTimer<pac::TIM1> = ();
@@ -80,10 +84,13 @@ const APP: () = {
             .pc13
             .into_push_pull_output_with_state(&mut gpioc.crh, State::High);
 
-        // Configure the syst timer to trigger an update every second and enables interrupt
         let mut timer =
-            Timer::tim1(cx.device.TIM1, &clocks, &mut rcc.apb2).start_count_down(1.hz());
+            Timer::tim1(cx.device.TIM1, &clocks, &mut rcc.apb2).start_count_down(10.hz());
         timer.listen(Event::Update);
+
+        let mut timer2 =
+            Timer::tim2(cx.device.TIM2, &clocks, &mut rcc.apb1).start_count_down(1.hz());
+        timer2.listen(Event::Update);
 
         let sck = gpiob.pb13.into_alternate_push_pull(&mut gpiob.crh);
         let miso = gpiob.pb14.into_floating_input(&mut gpiob.crh);
@@ -116,17 +123,24 @@ const APP: () = {
         let spi_dma = spi.with_tx_dma(dma.5);
 
         // Start a DMA transfer
-        let (_buffer, spi_dma) = unsafe { spi_dma.write(&DATA).wait() };
+        // let (_buffer, spi_dma) = unsafe { spi_dma.write(&DATA).wait() };
+
+        let spi_dma = singleton!(: DmaInterface = spi_dma).unwrap();
+
+        // let buf = singleton!(: [u8; DATA_LEN] = [OFF_BYTE; DATA_LEN]).unwrap();
+        unsafe { DATA[DATA.len() - 1] = 0x00 };
 
         // Init the static resources to use them later through RTFM
         init::LateResources {
             led: led,
             spi_dma: spi_dma,
             timer_handler: timer,
+            timer2_handler: timer2,
+            // buf,
         }
     }
 
-    #[idle(resources = [spi_dma])]
+    #[idle(resources = [ spi_dma])]
     fn idle(cx: idle::Context) -> ! {
         loop {
             // Waits for interrupt
@@ -134,11 +148,51 @@ const APP: () = {
         }
     }
 
-    #[task(binds = TIM1_UP, resources = [led, timer_handler])]
-    fn tick(cx: tick::Context) {
+    #[task(binds = TIM1_UP, resources = [spi_dma, led, timer_handler])]
+    fn frame_update(cx: frame_update::Context) {
         cx.resources.led.toggle();
+
+        use core::sync::atomic::{self, Ordering};
+        use stm32f1xx_hal::dma::TransferPayload;
+
+        let mut spi_dma: &mut DmaInterface = cx.resources.spi_dma;
+
+        unsafe {
+            spi_dma
+                .channel
+                .set_peripheral_address(unsafe { &(*SPI2::ptr()).dr as *const _ as u32 }, false);
+            spi_dma
+                .channel
+                .set_memory_address(DATA.as_ptr() as u32, true);
+            spi_dma.channel.set_transfer_length(DATA.len());
+        }
+        atomic::compiler_fence(Ordering::Release);
+        spi_dma.channel.ch().cr.modify(|_, w| {
+            w.mem2mem()
+                .clear_bit()
+                .pl()
+                .medium()
+                .msize()
+                .bits8()
+                .psize()
+                .bits8()
+                .circ()
+                .clear_bit()
+                .dir()
+                .set_bit()
+        });
+        spi_dma.start();
 
         // Clears the update flag
         cx.resources.timer_handler.clear_update_interrupt_flag();
+    }
+
+    #[task(binds = TIM2, resources = [timer2_handler])]
+    fn animation_update(cx: animation_update::Context) {
+        // cx.resources.led.toggle();
+        unsafe { DATA[7] = ON_BYTE };
+
+        // Clears the update flag
+        cx.resources.timer2_handler.clear_update_interrupt_flag();
     }
 };
