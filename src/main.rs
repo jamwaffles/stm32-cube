6eb2fdc824465c02efad3678567da1b73ffa2a21
James Waples <james@wapl.es>
2019-12-25 13:50:52 +0000

    Show R G and B LEDs for testing

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@
 
 use panic_semihosting as _;
 
+use core::ptr;
 use cortex_m::{asm::wfi, singleton};
-use rtfm::app;
-
 use embedded_hal::digital::v2::OutputPin;
+use rtfm::app;
 use stm32f1xx_hal::{
     device::SPI2,
     dma,
@@ -41,6 +41,13 @@ type DmaInterface = stm32f1xx_hal::dma::TxDma<
 
 const DATA_LEN: usize = (64 * 8 * 3) + 1;
 static mut DATA: [u8; DATA_LEN] = [OFF_BYTE; DATA_LEN];
+
+#[derive(Copy, Clone)]
+pub struct Apa106Led {
+    pub red: u8,
+    pub green: u8,
+    pub blue: u8,
+}
 
 #[app(device = stm32f1xx_hal::pac, peripherals = true)]
 const APP: () = {
@@ -150,7 +157,7 @@ const APP: () = {
 
     #[task(binds = TIM1_UP, resources = [spi_dma, led, timer_handler])]
     fn frame_update(cx: frame_update::Context) {
-        cx.resources.led.toggle();
+        // cx.resources.led.toggle();
 
         use core::sync::atomic::{self, Ordering};
         use stm32f1xx_hal::dma::TransferPayload;
@@ -182,17 +189,105 @@ const APP: () = {
                 .set_bit()
         });
         spi_dma.start();
+        while spi_dma.channel.in_progress() {}
+
+        atomic::compiler_fence(Ordering::Acquire);
+
+        spi_dma.stop();
+
+        // we need a read here to make the Acquire fence effective
+        // we do *not* need this if `dma.stop` does a RMW operation
+        unsafe {
+            ptr::read_volatile(&0);
+        }
+
+        // we need a fence here for the same reason we need one in `Transfer.wait`
+        atomic::compiler_fence(Ordering::Acquire);
 
         // Clears the update flag
         cx.resources.timer_handler.clear_update_interrupt_flag();
     }
 
-    #[task(binds = TIM2, resources = [timer2_handler])]
+    #[task(binds = TIM2, resources = [led, timer2_handler])]
     fn animation_update(cx: animation_update::Context) {
-        // cx.resources.led.toggle();
+        cx.resources.led.toggle();
         unsafe { DATA[7] = ON_BYTE };
+
+        let lights = [
+            Apa106Led {
+                red: 0,
+                green: 10,
+                blue: 0,
+            },
+            Apa106Led {
+                red: 10,
+                green: 0,
+                blue: 0,
+            },
+            Apa106Led {
+                red: 0,
+                green: 0,
+                blue: 10,
+            },
+        ];
+
+        for (led_idx, light) in lights.iter().enumerate() {
+            let start = led_idx * (8 * 3);
+
+            for (byte_idx, bit) in colour_to_raw(&light).iter().enumerate() {
+                unsafe { DATA[start + byte_idx] = *bit }
+            }
+        }
 
         // Clears the update flag
         cx.resources.timer2_handler.clear_update_interrupt_flag();
     }
 };
+
+fn bit_is_set(byte: u8, bit_index: u8) -> bool {
+    (byte & (1 << bit_index)) != 0
+}
+
+fn colour_to_raw(input: &Apa106Led) -> [u8; 24] {
+    let mut bytes: [u8; 24] = [0; 24];
+
+    // Gamma correct colours
+    // let gamma_corrected_input = Apa106Led {
+    //  red: GAMMA_MAP[input.red as usize],
+    //  green: GAMMA_MAP[input.green as usize],
+    //  blue: GAMMA_MAP[input.blue as usize],
+    // };
+
+    // SPI transmits MSB first
+    // Gamma correction
+    // for pos in 0..8 {
+    //  bytes[7 - pos as usize] = if bit_is_set(gamma_corrected_input.red, pos as u8) { ON_BYTE } else { OFF_BYTE };
+
+    //  bytes[8 + (7 - pos as usize)] = if bit_is_set(gamma_corrected_input.green, pos as u8) { ON_BYTE } else { OFF_BYTE };
+
+    //  bytes[16 + (7 - pos as usize)] = if bit_is_set(gamma_corrected_input.blue, pos as u8) { ON_BYTE } else { OFF_BYTE };
+    // }
+
+    // No gamma correction
+    for pos in 0..8 {
+        bytes[7 - pos as usize] = if bit_is_set(input.red, pos as u8) {
+            ON_BYTE
+        } else {
+            OFF_BYTE
+        };
+
+        bytes[8 + (7 - pos as usize)] = if bit_is_set(input.green, pos as u8) {
+            ON_BYTE
+        } else {
+            OFF_BYTE
+        };
+
+        bytes[16 + (7 - pos as usize)] = if bit_is_set(input.blue, pos as u8) {
+            ON_BYTE
+        } else {
+            OFF_BYTE
+        };
+    }
+
+    bytes
+}
