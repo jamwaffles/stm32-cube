f9597088d7bb046a1dbdb0b2935912edd2217ae2
James Waples <james@wapl.es>
2020-12-04 17:40:23 +0000

    Add common skeleton

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,3 +1,91 @@
-fn main() {
-    println!("Hello, world!");
+//! A debugging tool for thick lines
+//!
+//! Use the up/down arrow keys to increase or decrease the line thickness. Click and drag to move
+//! the end point of the line around.
+//!
+//! The thickness, DX and DY components of the line are displayed in the top right corner of the
+//! window.
+
+extern crate embedded_graphics;
+extern crate embedded_graphics_simulator;
+
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    pixelcolor::Rgb888,
+    prelude::*,
+    primitives::Line,
+    primitives::{common::LineJoin, common::StrokeOffset, line::Intersection, Polyline},
+    style::{MonoTextStyle, PrimitiveStyle},
+};
+use embedded_graphics_simulator::{
+    OutputSettingsBuilder, SimulatorDisplay, SimulatorEvent, Window,
+};
+use sdl2::keyboard::Keycode;
+
+fn draw_layer(display: &mut SimulatorDisplay<Rgb888>) -> Result<(), core::convert::Infallible> {
+    //
+
+    Ok(())
+}
+
+fn draw(
+    display: &mut SimulatorDisplay<Rgb888>,
+    position: Point,
+    stroke_width: u32,
+) -> Result<(), core::convert::Infallible> {
+    display.clear(Rgb888::CSS_DARK_GRAY)?;
+
+    //
+
+    Ok(())
+}
+
+fn main() -> Result<(), core::convert::Infallible> {
+    let mut display: SimulatorDisplay<Rgb888> = SimulatorDisplay::new(Size::new(700, 200));
+    let output_settings = OutputSettingsBuilder::new()
+        .scale(2)
+        // .pixel_spacing(1)
+        .build();
+    let mut window = Window::new("Cube sim", &output_settings);
+
+    let mut position = Point::new(29, 30);
+    let mut stroke_width = 5;
+    let mut mouse_down = false;
+
+    draw(&mut display, position, stroke_width)?;
+
+    'running: loop {
+        window.update(&display);
+
+        for event in window.events() {
+            match event {
+                SimulatorEvent::Quit => break 'running,
+                SimulatorEvent::KeyDown { keycode, .. } => {
+                    match keycode {
+                        Keycode::Up => stroke_width += 1,
+                        Keycode::Down => stroke_width = (stroke_width as i32 - 1).max(0) as u32,
+                        _ => (),
+                    }
+
+                    draw(&mut display, position, stroke_width)?;
+                }
+                SimulatorEvent::MouseButtonDown { point, .. } => {
+                    mouse_down = true;
+                    position = point;
+
+                    draw(&mut display, position, stroke_width)?;
+                }
+                SimulatorEvent::MouseButtonUp { .. } => mouse_down = false,
+                SimulatorEvent::MouseMove { point, .. } => {
+                    if mouse_down {
+                        position = point;
+                        draw(&mut display, position, stroke_width)?;
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    Ok(())
 }
