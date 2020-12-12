d01589e588d77c737cb6c53ed26143934c8168ea
James Waples <james@wapl.es>
2020-12-12 20:05:40 +0000

    Make column switching work with offsets

--- common/src/patterns/slow_rain.rs
+++ common/src/patterns/slow_rain.rs
@@ -1,6 +1,5 @@
 use crate::{
     apa106led::{Apa106Led, OFF, WARM_WHITE},
-    cube::Cube,
     patterns::PatternUpdate,
     voxel::Voxel,
 };
@@ -8,19 +7,25 @@ use core::f32::consts::PI;
 use micromath::F32Ext;
 use rand::prelude::*;
 
+#[derive(Clone, Copy, Debug, PartialEq)]
+enum Mask {
+    Visible,
+    Hidden,
+
+    /// Wait until column position is at zero, then enable visiblity
+    Stage,
+}
+
 #[derive(Clone, Debug)]
 pub struct SlowRain {
     /// How long a drop takes to go from the top to the bottom of the cube.
     drop_duration: u32,
 
-    /// Pattern cache.
-    cube: Cube,
-
     /// Each column gets an offset so drops don't all fall together.
     offsets: [u8; 16],
 
     /// Turn columns on or off.
-    mask: [bool; 16],
+    mask: [Mask; 16],
 
     rng: SmallRng,
 }
@@ -30,40 +35,34 @@ impl Default for SlowRain {
         let mut rng = SmallRng::seed_from_u64(0xdead_beef_cafe_babe);
 
         let mut offsets = [0u8; 16];
-        let mut mask = [false; 16];
+        let mut mask = [Mask::Hidden; 16];
 
         rng.fill_bytes(&mut offsets);
 
-        for (idx, column_on) in mask.iter_mut().enumerate() {
+        mask.iter_mut().for_each(|mask| {
             let i = rng.next_u32() % 16;
 
-            *column_on = i < 4;
-        }
+            *mask = if i < 4 { Mask::Visible } else { Mask::Hidden };
+        });
 
-        let mut self_ = Self {
+        Self {
             drop_duration: 2000,
-            cube: Cube::default(),
             rng,
             offsets,
             mask,
-        };
-
-        self_
+        }
     }
 }
 
 impl PatternUpdate for SlowRain {
-    type CycleCounter = u32;
+    type CycleCounter = ();
 
     fn pixel_at(&mut self, idx: usize, time: u32, _frame_delta: u32) -> Apa106Led {
         let voxel = Voxel::from_index(idx);
         let column_idx = (voxel.x + voxel.y * 4) as usize;
 
-        // Smaller values = drops closer together
-        let offset_mul = 1.0;
-
-        let column_offset = self.offsets[column_idx] as f32 / 255.0 * offset_mul;
-        let mask = self.mask[column_idx];
+        let column_offset = self.offsets[column_idx] as f32 / 255.0 - 127.0;
+        let mut mask = self.mask[column_idx];
 
         // Length in voxels away from leading point where brightness should be zero
         let tail_len = 3.0;
@@ -78,7 +77,7 @@ impl PatternUpdate for SlowRain {
         let time_pos = (time_pos + column_offset).rem_euclid(1.0);
 
         // Reset
-        if time_pos >= 0.99 && mask && voxel.z == 3 {
+        if time_pos >= 0.99 && mask == Mask::Visible && voxel.z == 3 {
             loop {
                 let next_idx = (self.rng.next_u32() as usize + column_idx) % 16;
 
@@ -88,14 +87,16 @@ impl PatternUpdate for SlowRain {
                 }
 
                 // Find next unlit column
-                if !self.mask[next_idx] {
-                    self.mask[next_idx] = true;
-                    self.mask[column_idx] = false;
-                    self.offsets[next_idx] = self.offsets[column_idx];
+                if self.mask[next_idx] == Mask::Hidden {
+                    self.mask[column_idx] = Mask::Hidden;
+                    self.mask[next_idx] = Mask::Stage;
 
                     break;
                 }
             }
+        } else if time_pos <= 0.01 && mask == Mask::Stage {
+            self.mask[column_idx] = Mask::Visible;
+            mask = Mask::Visible;
         }
 
         // Off the top of the cube by tail_len to below cube by tail_len
@@ -106,7 +107,7 @@ impl PatternUpdate for SlowRain {
         // Can check sign later for different leading/trailing behaviours
         let distance = voxel_pos - scaled_time_pos;
 
-        if mask {
+        if mask == Mask::Visible {
             // 1.0 - 0.0 clamped
             let distance = (distance / tail_len).abs().min(1.0);
 
@@ -119,7 +120,5 @@ impl PatternUpdate for SlowRain {
         }
     }
 
-    fn completed_cycles(&self, time: u32) -> Self::CycleCounter {
-        time / 5000
-    }
+    fn completed_cycles(&self, _time: u32) -> Self::CycleCounter {}
 }
